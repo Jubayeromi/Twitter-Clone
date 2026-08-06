0ec403f441e2f86923d78de323bb0483aceffa24
Jubayeromi <abdullaomi46@gmail.com>
2026-08-06 22:12:44 +0600

    i don't know

--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -8,26 +8,26 @@ const Second = () => {
     return (
 
         <div className="second flex w-1/3 border-x border-gray-300">
-            <div className="top flex transparent w-full flex-col">
-                <div className='flex'>
-<Link className='flex ' to="/">
-                <div className="left h-10 w-full text-center flex-1 justify-center items-center">
-                    For You
-                </div>
-</Link>
-                    <Link className='flex flex-1' to="/following">
-                <div className="right h-10 w-full text-center flex  justify-center items-center">
-                     Following
-                     </div>
+            <div className="top flex transparent w-full flex-col relative">
+                <div className='flex bg-black/70 backdrop:blur-3xl border sticky top-0 border-y-gray-600 '>
+                    <Link className='flex ' to="/">
+                        <div className="left flex px-2 h-10 w-fit text-center justify-center items-center">
+                            For You
+                        </div>
                     </Link>
-                    <div> <Settings /> </div>
+                    <Link className='flex flex-1' to="/following">
+                        <div className="right px-2 h-10 w-fit text-center flex  justify-center items-center">
+                            Following
+                        </div>
+                    </Link>
+                    <div className='flex justify-center items-center'> <Settings /> </div>
                 </div>
 
                 <Routes>
-                    <Route path='/' element={ <ForYou /> } />
+                    <Route path='/' element={<ForYou />} />
                     <Route path='/following' element={<Following />} />
                 </Routes>
-                
+
             </div>
         </div>
 
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,3 +1,4 @@
+import { ImagePlay, ImageUp, FaceSlightlySmiling } from 'lucide-react'
 import React from 'react'
 
 const Post = () => {
@@ -6,7 +7,19 @@ const Post = () => {
     <div className='border-t border-gray-600 h-40 flex'>
       <div className=''>
         <img className=' mt-3 ml-3 h-12 w-12 rounded-full object-top' src="/my imgae.jpg" alt="" /></div>
-      <textarea className='p-5 text-lg w-full h-40 border-none outline-none resize-none overflow-y-auto' name="" id="" placeholder="What's happening ?"></textarea>
+        <div className='flex flex-col'>
+
+      <textarea className='p-5 text-lg w-full h-30 border-none outline-none resize-none overflow-y-auto' name="" id="" placeholder="What's happening ?"></textarea>
+      <div className='ml-3 flex'>
+        <div className='p-2 rounded-full hover:bg-gray-600 h-fit w-fit hover:text-gray-200'>
+       <ImageUp  className='hover:text-gray-200 text-gray-400 cursor-pointer hover:scale-110 ' />
+        </div>
+         <div className='p-2 rounded-full hover:bg-gray-600 h-fit w-fit hover:text-gray-200'>
+       <ImagePlay  className='hover:text-gray-200 text-gray-400 cursor-pointer hover:scale-110 ' />
+        </div>
+        
+      </div>
+        </div>
     </div>
   )
 }
